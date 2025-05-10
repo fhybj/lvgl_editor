7676b83c61bcb38f99178b47924897b35aae7357
Binake <fhybj@outlook.com>
2025-05-10 12:38:09 +0800

    change checkbox color

--- examples/components/checkbox/checkbox_gen.c
+++ examples/components/checkbox/checkbox_gen.c
@@ -34,6 +34,8 @@ lv_obj_t * checkbox_create(lv_obj_t * parent, const char * text, lv_subject_t * 
     LV_TRACE_OBJ_CREATE("begin");
 
     static lv_style_t style_box;
+    static lv_style_t style_main;
+    static lv_style_t style_bs;
 
     static bool style_inited = false;
 
@@ -43,11 +45,20 @@ lv_obj_t * checkbox_create(lv_obj_t * parent, const char * text, lv_subject_t * 
         lv_style_set_transform_height(&style_box, 0);
         lv_style_set_transform_width(&style_box, 0);
 
+        lv_style_init(&style_main);
+        lv_style_set_border_color(&style_main, lv_color_hex(0xffa020));
+
+        lv_style_init(&style_bs);
+        lv_style_set_bg_color(&style_bs, lv_color_hex(0xffa020));
+        lv_style_set_border_color(&style_bs, lv_color_hex(0xffa020));
+
         style_inited = true;
     }
 
     lv_obj_t * lv_checkbox_1 = lv_checkbox_create(parent);
-        lv_obj_add_style(lv_checkbox_1, &style_box, LV_PART_INDICATOR | LV_STATE_PRESSED);
+        lv_obj_add_style(lv_checkbox_1, &style_main, LV_PART_INDICATOR | LV_STATE_DEFAULT);
+    lv_obj_add_style(lv_checkbox_1, &style_box, LV_PART_INDICATOR | LV_STATE_PRESSED);
+    lv_obj_add_style(lv_checkbox_1, &style_bs, LV_PART_INDICATOR | LV_STATE_CHECKED);
     
     lv_obj_bind_checked(lv_checkbox_1, subject);
     lv_checkbox_set_text(lv_checkbox_1, text);
